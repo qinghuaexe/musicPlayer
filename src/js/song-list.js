95d911e212776ed2d33670cff211de37c9a6aa2f
qinghuaexe <qinghuaexe@gmail.com>
2019-03-02 15:36:07 +0800

    新建时清空数据

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -43,6 +43,7 @@
             this.model = model
             this.view.render(this.model.data)
             this.bindEvents()
+            this.bindEventHub()
             this.getAllSongs()
         },
         getAllSongs(){
@@ -72,6 +73,9 @@
             window.eventHub.on('create', (songData) => {
                 this.model.data.songs.push(songData)
                 this.view.render(this.model.data)
+            })
+            window.eventHub.on('new',()=>{
+                this.view.clearActive()
             })
         }
     }
