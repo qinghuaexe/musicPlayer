5e87aa63c35366c061e37b86f2a23274f01c0933
qinghuaexe <qinghuaexe@gmail.com>
2019-03-12 21:04:31 +0800

    添加播放页面样式并实现播放暂停功能

--- src/js/admin/song-form.js
+++ src/js/admin/song-form.js
@@ -20,9 +20,15 @@
                 </div>
                 <div class="row">
                     <label>
-                        外链
+                        链接
                     </label>
                     <input  name="url" type="text" value="__url__">
+                </div>
+                <div class="row">
+                    <label>
+                        封面
+                    </label>
+                    <input  name="cover" type="text" value="__cover__">
                 </div>
                 <div class="row actions">
                     <button type="submit">保存</button>
@@ -30,7 +36,7 @@
             </form>
         `,
         render(data = {}) {
-            let placeholders = ['name', 'url', 'singer', 'id']
+            let placeholders = ['name', 'url', 'singer', 'id','cover']
             let html = this.template
             placeholders.map((string) => {
                 html = html.replace(`__${string}__`, data[string] || '')
@@ -47,12 +53,13 @@
         }
     }
     let model = {
-        data: { name: '', singer: '', url: '', id: '' },
+        data: { name: '', singer: '', url: '', id: '',cover:'' },
         update(data){
             var song = AV.Object.createWithoutData('Song', this.data.id);
             song.set('name', data.name)
             song.set('singer', data.singer)
             song.set('url', data.url)
+            song.set('cover', data.cover)
             return song.save().then((response)=>{
                 Object.assign(this.data,data)
                 return response
@@ -67,6 +74,7 @@
             Song.set('name', data.name);
             Song.set('singer', data.singer);
             Song.set('url', data.url);
+            song.set('cover', data.cover)
             return Song.save().then((newSong) => {
                 let { id, attributes} = newSong
                 Object.assign(this.data,{id,...attributes})
@@ -96,7 +104,7 @@
             })
         },
         create(){
-            let needs = 'name singer url'.split(' ')
+            let needs = 'name singer url cover'.split(' ')
             let data = {}
             needs.map((string) => {
                 data[string] = this.view.$el.find(`[name="${string}"]`).val()
@@ -110,7 +118,7 @@
                 })
         },
         update(){
-            let needs = 'name singer url'.split(' ')
+            let needs = 'name singer url cover'.split(' ')
             let data = {}
             needs.map((string) => {
                 data[string] = this.view.$el.find(`[name="${string}"]`).val()
